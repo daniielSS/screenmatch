a9e5524e072f3b5f8cfacbb81eeb008da718b4fe
Daniel Silva <danielss.g8@gmail.com>
2026-07-06 19:38:15 -0300

    initial project

---
+++ src/main/java/br/com/daniel/screenmatch/controllers/HelloWorldController.java
@@ -0,0 +1,12 @@
+package br.com.daniel.screenmatch.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping(path = "/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
